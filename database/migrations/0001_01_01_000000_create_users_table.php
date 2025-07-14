855f8d14547b98e86ee523dbeea28322d0837d50
tasFiquejim <tasfiquejim@gmail.com>
2025-07-14 22:43:23 +0600

    feat: Implement user authentication and profile management with Livewire components

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
             $table->id();
             $table->string('first_name');
             $table->string('last_name');
-            $table->string('username')->unique();
+            $table->string('username')->unique()->nullable();
             $table->string('email')->unique();
             $table->string('phone')->nullable();
             $table->string('address')->nullable();
